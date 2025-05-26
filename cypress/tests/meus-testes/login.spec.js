72429ac41ad50f14f38b2b5f77990167806cb2dd
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-05-26 03:53:30 -0300

    CT01 usando cypress fixtures e baseUrl configurada

--- cypress/tests/meus-testes/login.spec.js
+++ cypress/tests/meus-testes/login.spec.js
@@ -1,4 +1,4 @@
-
+import userData from '../../fixtures/userData.json'; // importação dos dados do usuário do arquivo JSON
 
 describe('Testes de Login', () => {
     const seletoresLogin = {
@@ -11,8 +11,8 @@ describe('Testes de Login', () => {
 
     it('Login com sucesso', () => {
         cy.visit('/signin');
-        cy.get(seletoresLogin.campoUsername).type('Heath93') // preenche o campo username
-        cy.get(seletoresLogin.campoPassword).type('s3cret') // preenche o campo password
+        cy.get(seletoresLogin.campoUsername).type(userData.usuarioSucesso.username) // preenche o campo username
+        cy.get(seletoresLogin.campoPassword).type(userData.usuarioSucesso.password) // preenche o campo password
         cy.get(seletoresLogin.checkboxRemember).check() // clica no checkbox
         cy.get(seletoresLogin.btnSignIn).click() // clica no botão de login
         cy.get(seletoresLogin.abaEveryoneHome).should('contain', 'Everyone') // verifica se a aba Everyone Home está visível
