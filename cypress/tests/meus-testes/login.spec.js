7b75e96ccf5d027c39ba3f5f6c30f38e5c2538cc
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-05-26 03:57:49 -0300

    CT02: Login com falha - senha inválida

--- cypress/tests/meus-testes/login.spec.js
+++ cypress/tests/meus-testes/login.spec.js
@@ -7,6 +7,7 @@ describe('Testes de Login', () => {
         checkboxRemember: "[name='remember']",
         btnSignIn: "[type='submit']",
         abaEveryoneHome: "[tabindex='0']",
+        msgErroLogin: "[role='alert']",
     }
 
     it('Login com sucesso', () => {
@@ -16,5 +17,14 @@ describe('Testes de Login', () => {
         cy.get(seletoresLogin.checkboxRemember).check() // clica no checkbox
         cy.get(seletoresLogin.btnSignIn).click() // clica no botão de login
         cy.get(seletoresLogin.abaEveryoneHome).should('contain', 'Everyone') // verifica se a aba Everyone Home está visível
+    });
+
+    it('Login com falha - senha inválido', () => {
+        cy.visit('/signin');
+        cy.get(seletoresLogin.campoUsername).type(userData.usuarioSucesso.username) // preenche o campo username
+        cy.get(seletoresLogin.campoPassword).type(userData.usuarioFalha.password) // preenche o campo password com dado inválido
+        cy.get(seletoresLogin.checkboxRemember).check() // clica no checkbox
+        cy.get(seletoresLogin.btnSignIn).click() // clica no botão de login
+        cy.get(seletoresLogin.msgErroLogin).should('contain', 'Username or password is invalid') // verifica se a mensagem de erro está visível
     });
 });
